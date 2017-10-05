1f128d4b44ebbbf4cea7f802480d2deb4661370f
Sotoumi <lokki_y@yahoo.co.jp>
2017-10-05 12:57:52 +0000

    add s3 config

--- Gemfile
+++ Gemfile
@@ -20,6 +20,7 @@ gem 'certified'
 gem "figaro","~>1.1.0"
 gem "paperclip", "~> 5.0.0"
 gem 'dropzonejs-rails'
+gem 'aws-sdk', '~> 2.3'
 
 group :production do
   gem 'pg'
---
+++ config/environments/paperclip.rb
@@ -0,0 +1,3 @@
+Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+Paperclip::Attachment.default_options[:s3_ap_northeast-1] = 's3-us-west-2.amazonaws.com'
